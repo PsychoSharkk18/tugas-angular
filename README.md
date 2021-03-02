2e8ac4682c59058d30261252bf481e89fd74adc4
PsychoSharkk18 <c14180047@john.petra.ac.id>
2021-03-02 20:02:31 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-nkp6gk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-nkp6gk)
---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, VERSION } from "@angular/core";
+import { Router } from "@angular/router";
+import { GlobalvarService } from "./globalvar.service";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  constructor(private router: Router, public globalvar: GlobalvarService) {
+    this.namaglobal = this.globalvar.getnama();
+  }
+
+  textnama = "";
+  textisi=""
+  name = "Angular" + VERSION.major;
+  namaglobal;
+  OPENHALLOGIN() {
+    this.router.navigate(["/home/" + this.textnama]);
+  }
+}
